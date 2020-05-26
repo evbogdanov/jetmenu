793ec8541041912195d5a3da7522b3ab6197903c
Ev Bogdanov <evbogdanov.com@gmail.com>
2020-05-26 22:39:02 +0300

    CORS

--- server/index.js
+++ server/index.js
@@ -2,15 +2,23 @@ const fs = require('fs')
 const path = require('path')
 const http = require('http')
 
+const HEADER_CONTENT_TYPE = {
+  'Content-Type': 'application/json; charset=UTF-8',
+}
+const HEADER_CORS = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'OPTIONS, GET',
+}
+
 const pathToFile = path.join(__dirname, 'data.json')
 const data = fs.readFileSync(pathToFile)
 
 const server = http.createServer((req, res) => {
   if (req.url === '/data') {
-    res.writeHead(200, { 'Content-Type': 'application/json; charset=UTF-8' })
+    res.writeHead(200, {...HEADER_CONTENT_TYPE, ...HEADER_CORS})
     res.end(data)
   } else {
-    res.writeHead(404)
+    res.writeHead(404, HEADER_CORS)
     res.end('Not Found')
   }
 })

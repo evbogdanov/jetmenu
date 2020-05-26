9018cf676ac432a9927bca38468f0c3e80544631
Ev Bogdanov <evbogdanov.com@gmail.com>
2020-05-26 20:57:38 +0300

    Initial Menu component

---
+++ client/components/Menu/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Nav } from './styled'
+
+function Menu() {
+  return <Nav>Hello, Menu</Nav>
+}
+
+export default Menu
---
+++ client/components/Menu/styled.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+const navBorder = '1px solid #e6e6e6'
+
+export const Nav = styled.nav`
+  width: 300px;
+  background-color: #fafafa;
+  border-right: ${navBorder};
+  border-left: ${navBorder};
+`

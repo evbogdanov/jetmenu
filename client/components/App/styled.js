aaa40d6ceece4fa2c2e5f9976169daf1e603ae40
Ev Bogdanov <evbogdanov.com@gmail.com>
2020-05-26 21:29:33 +0300

    Open Sans font

--- client/components/App/styled.js
+++ client/components/App/styled.js
@@ -8,6 +8,11 @@ export const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
   }
+
+  body {
+    font-family: 'Open Sans', sans-serif;
+    font-size: 14px;
+  }
 `
 
 export const Main = styled.main`
